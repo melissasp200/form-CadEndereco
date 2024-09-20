ea9844d32d388a1c19ea1e579156a067f9ee352f
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-09-20 11:09:11 -0300

    começo do js

---
+++ cade.js
@@ -0,0 +1,13 @@
+'use strict' //Ativa o modo restrito.
+             // Este modo faz com que o JS opere de forma mais segura e rigorosa, ajudando a evitar erros comuns de progamação /*Consumo de API - https://viacep.com.br/ */
+
+//Função para limpar campos preenchidos
+const limparFormulario = () => {
+
+  document.getElementById('logradouro').value = '';
+  document.getElementById('localidade').value = '';
+  document.getElementById('uf').value = '';
+  document.getElementById('bairro').value = '';
+  document.getElementById('complemento').value = '';
+
+}
